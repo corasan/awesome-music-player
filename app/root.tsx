533c6653e88d429bb1ad463e982c8d6b0f758c24
Henry Paulino <corasan.dev@gmail.com>
2022-04-02 18:14:29 -0400

    Add musickit script tag

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,11 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { MetaFunction } from '@remix-run/node'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+})
 
 export default function App() {
   return (
@@ -26,7 +19,8 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <script src="https://js-cdn.music.apple.com/musickit/v1/musickit.js" />
       </body>
     </html>
-  );
+  )
 }
