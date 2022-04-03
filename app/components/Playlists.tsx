65d586736704eb9fcc2248a554ae088c9b464239
Henry Paulino <corasan.dev@gmail.com>
2022-04-03 18:05:20 -0400

    Create Playlists.tsx

---
+++ app/components/Playlists.tsx
@@ -0,0 +1,20 @@
+import { Button, Grid, Link } from '@nextui-org/react'
+import { observer } from 'mobx-react-lite'
+
+const Playlists = ({ data }: { data: MusicKit.Playlist[] }) => {
+  return (
+    <Grid xs={2.2} css={{ height: '100%', p: 15 }} direction="column">
+      {data.map(p => (
+        <PlaylistItem key={p.id} label={p.attributes.name} />
+      ))}
+    </Grid>
+  )
+}
+
+const PlaylistItem = ({ label }: { label: string }) => (
+  <Link block color="primary">
+    {label}
+  </Link>
+)
+
+export default observer(Playlists)
