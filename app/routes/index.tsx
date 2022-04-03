dabb69d2f839e625fec116d0227fa5c511491acb
Henry Paulino <corasan.dev@gmail.com>
2022-04-03 00:37:33 -0400

    authorize musicKit

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,29 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useSubmit } from '@remix-run/react'
 import { useStore } from '~/stores'
 import { observer } from 'mobx-react-lite'
+import setupMusicKit from '~/util/setupMusicKit'
 
 const Index = observer(() => {
   const submit = useSubmit()
-  const { developerToken } = useStore()
+  const { developerToken, setMusicKitInstance, musicKit } = useStore()
+
+  const reqMusicKit = useCallback(async (token: string) => {
+    const kit = await setupMusicKit(token)
+    const res = await kit.authorize()
+    console.log(res)
+    setMusicKitInstance(kit)
+  }, [])
+
+  const authorize = useCallback(async () => {
+    await musicKit.authorize()
+  }, [])
 
   useEffect(() => {
     if (!developerToken) {
       submit(null, { method: 'post', action: '/setup' })
+    } else {
+      reqMusicKit(developerToken)
     }
   }, [developerToken, submit])
 
