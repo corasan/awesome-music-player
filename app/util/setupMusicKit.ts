4ec7e6ca33db8fd5fb3eec3ca9076d587c3b1b43
Henry Paulino <corasan.dev@gmail.com>
2022-04-02 22:58:00 -0400

    Create setupMusicKit.ts

---
+++ app/util/setupMusicKit.ts
@@ -0,0 +1,13 @@
+export default function setupMusicKit(developerToken: string) {
+  return new Promise(resolve => {
+    const musicKitInstance = window.MusicKit.configure({
+      developerToken,
+      app: {
+        name: 'Awesome Music Player',
+        build: '1.0.0',
+      },
+    })
+    delete window.MusicKit // clear global scope
+    resolve(musicKitInstance)
+  })
+}
