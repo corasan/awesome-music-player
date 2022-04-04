11f2e4b83629048469e1d4ad39644fb677fae6e2
Henry Paulino <corasan.dev@gmail.com>
2022-04-04 14:02:04 -0400

    misc

--- app/stores/PlayerStore.ts
+++ app/stores/PlayerStore.ts
@@ -14,16 +14,16 @@ export default class PlayerStore {
     this.musicKit = rootStore.musicKit
   }
 
-  async getPlayerInstance() {
-    const res = this.musicKit.instance?.player ?? null
-  }
-
   get p() {
     return this.musicKit.instance?.player
   }
 
   get playbackProgress() {
     return this.p?.currentPlaybackProgress ?? 0
+  }
+
+  get playbackDuration() {
+    return this.p?.currentPlaybackDuration
   }
 
   setNowPlaying(value: MusicKit.MediaItem) {
