3263ef02c07ef940d16aef8b81abc0663e8756e0
Henry Paulino <corasan.dev@gmail.com>
2022-04-04 16:13:40 -0400

    handle logic in mobx

--- app/components/PlayerControls.tsx
+++ app/components/PlayerControls.tsx
@@ -1,44 +1,33 @@
 import { Progress, Grid, Text } from '@nextui-org/react'
 import { observer } from 'mobx-react-lite'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useStore } from '~/stores'
 import formatSeconds from '~/util/formatSeconds'
 
 const PlayerControls = () => {
-  const { player, musicKit } = useStore()
-  const [currentTime, setCurrentTime] = useState(0)
-  const duration = player.p?.currentPlaybackDuration ?? 0
+  const { player } = useStore()
+  const duration = player.playbackDuration / 1000
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (player.p?.nowPlayingItem) {
-        setCurrentTime(prev => prev + 1)
-      }
-    }, 1000)
-
-    return () => clearTimeout(interval)
-  }, [musicKit.instance])
-
-  useEffect(() => {
-    if (player.p?.nowPlayingItem) {
+    if (player.nowPlaying) {
       player.startProgress()
-    } else {
-      player.stopProgress()
+      player.startTime()
     }
-    return () => player.stopProgress()
-  }, [player.p?.nowPlayingItem])
+
+    if (player.playbackTime >= player.playbackDuration) {
+      player.reset()
+    }
+
+    return () => player.reset()
+  }, [player.nowPlaying])
 
   return (
     <Grid.Container direction="row" justify="space-evenly" alignItems="center">
       <Grid xs={2} justify="center">
-        {!player.p?.nowPlayingItem ? (
-          <Text>-:--</Text>
-        ) : (
-          <Text>{formatSeconds(player.playbackTime)}</Text>
-        )}
+        {!player.nowPlaying ? <Text>-:--</Text> : <Text>{formatSeconds(player.playbackTime)}</Text>}
       </Grid>
       <Grid xs={8}>
-        {!player.p?.nowPlayingItem ? (
+        {!player.nowPlaying ? (
           <Progress value={0} color="gradient" size="xs" min={0} max={100} />
         ) : (
           <Progress
@@ -51,7 +40,7 @@ const PlayerControls = () => {
         )}
       </Grid>
       <Grid xs={2} justify="center">
-        <Text>{duration ? formatSeconds(duration) : '-:--'}</Text>
+        <Text>{duration !== 0 ? formatSeconds(duration) : '-:--'}</Text>
       </Grid>
     </Grid.Container>
   )
