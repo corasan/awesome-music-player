51057b8383a569e25b39f7aaac1a5b820bd3c786
Henry Paulino <corasan.dev@gmail.com>
2022-04-03 20:03:17 -0400

    better instance loading

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,18 @@
 import type { MetaFunction } from '@remix-run/node'
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from '@remix-run/react'
 import { NextUIProvider } from '@nextui-org/react'
-import StoreProvider from '~/stores'
+import StoreProvider, { useStore } from '~/stores'
+import getDeveloperToken from './util/getDeveloperToken'
+import { useEffect } from 'react'
+import { observer } from 'mobx-react-lite'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -9,23 +20,62 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1',
 })
 
-export default function App() {
+export const loader = async () => {
+  const TEAM_ID = process.env.TEAM_ID as string
+  const KEY_ID = process.env.KEY_ID as string
+  const PRIVATE_KEY = process.env.PRIVATE_KEY as string
+  const developerToken = getDeveloperToken(TEAM_ID, KEY_ID, PRIVATE_KEY)
+
+  return { developerToken }
+}
+
+const App = observer(() => {
+  const data = useLoaderData()
+  const { musicKit } = useStore()
+
+  const setup = async () => {
+    const instance = await window.MusicKit.configure({
+      developerToken: data.developerToken,
+      app: {
+        name: 'My Cool Web App',
+        build: '1978.4.1',
+      },
+    })
+    musicKit.setInstance(instance)
+  }
+
+  useEffect(() => {
+    if (!musicKit.instance) {
+      setup()
+    }
+  }, [data, musicKit.instance])
+
+  useEffect(() => {
+    if (musicKit.instance) {
+      musicKit.authorize()
+    }
+  }, [musicKit.instance])
+
+  return <Outlet />
+})
+
+export default function Root() {
   return (
     <html lang="en" style={{ display: 'flex', flex: 1, height: '100%' }}>
       <head>
         <Meta />
         <Links />
+        <script src="https://js-cdn.music.apple.com/musickit/v1/musickit.js" />
       </head>
       <body style={{ display: 'flex', flex: 1, height: '100%' }}>
         <NextUIProvider>
           <StoreProvider>
-            <Outlet />
+            <App />
           </StoreProvider>
         </NextUIProvider>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
-        <script src="https://js-cdn.music.apple.com/musickit/v1/musickit.js" />
       </body>
     </html>
   )
