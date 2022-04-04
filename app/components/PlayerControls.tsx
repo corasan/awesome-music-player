7b879d3a88de6d643879fdcd6c54b067986c7333
Henry Paulino <corasan.dev@gmail.com>
2022-04-04 14:01:34 -0400

    fix

--- app/components/PlayerControls.tsx
+++ app/components/PlayerControls.tsx
@@ -1,26 +1,38 @@
 import { Progress, Grid, Text } from '@nextui-org/react'
 import { observer } from 'mobx-react-lite'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useStore } from '~/stores'
 import formatSeconds from '~/util/formatSeconds'
 
-const PlayerControls = (props: any) => {
+const PlayerControls = () => {
   const { player, musicKit } = useStore()
   const [currentProgress, setCurrentProgress] = useState(0)
   const [currentTime, setCurrentTime] = useState(0)
-  const duration = player.p?.currentPlaybackDuration
+  const duration = player.p?.currentPlaybackDuration ?? 0
 
   useEffect(() => {
-    const timeout = setInterval(() => {
+    const interval = setInterval(() => {
       if (player.p?.nowPlayingItem) {
-        setCurrentTime(player.p.currentPlaybackTime)
-        setCurrentProgress(() => player.playbackProgress)
+        setCurrentTime(prev => prev + 1)
       }
     }, 1000)
 
-    return () => clearTimeout(timeout)
+    return () => clearTimeout(interval)
   }, [musicKit.instance])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (player.p?.nowPlayingItem) {
+        if (currentTime < duration) {
+          setCurrentProgress(prev => prev + 0.105)
+        } else {
+          setCurrentProgress(0)
+          setCurrentTime(0)
+        }
+      }
+    }, 100)
+    return () => clearInterval(interval)
+  }, [player.p?.nowPlayingItem, currentProgress])
 
   return (
     <Grid.Container direction="row" justify="space-evenly" alignItems="center">
@@ -28,7 +40,7 @@ const PlayerControls = (props: any) => {
         <Text>{currentTime === 0 ? '-:--' : formatSeconds(currentTime)}</Text>
       </Grid>
       <Grid xs={8}>
-        <Progress value={currentProgress * 100} color="gradient" size="xs" />
+        <Progress value={currentProgress} color="gradient" size="xs" min={0} max={duration} />
       </Grid>
       <Grid xs={2} justify="center">
         <Text>{duration ? formatSeconds(duration) : '-:--'}</Text>
