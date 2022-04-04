a140a535a2a65371c375ae96de0843c46f57c801
Henry Paulino <corasan.dev@gmail.com>
2022-04-03 20:03:24 -0400

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,47 +1,18 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router'
 import { observer } from 'mobx-react-lite'
-import { useLoaderData } from '@remix-run/react'
 import { Container, Grid, Loading } from '@nextui-org/react'
 import { useStore } from '~/stores'
-import setupMusicKit from '~/util/setupMusicKit'
-import getDeveloperToken from '~/util/getDeveloperToken'
 
-export const loader = async () => {
-  const TEAM_ID = process.env.TEAM_ID as string
-  const KEY_ID = process.env.KEY_ID as string
-  const PRIVATE_KEY = process.env.PRIVATE_KEY as string
-  const developerToken = getDeveloperToken(TEAM_ID, KEY_ID, PRIVATE_KEY)
-
-  return { developerToken }
-}
-
-const Index = observer(() => {
-  const data = useLoaderData()
+const Index = () => {
   const { musicKit } = useStore()
   const navigate = useNavigate()
-
-  const authorize = async () => {
-    await musicKit.authorize()
-  }
-
-  const setup = async (token: string) => {
-    const instance = await setupMusicKit(token)
-    musicKit.setInstance(instance)
-    await authorize()
-  }
-
-  useEffect(() => {
-    if (data.developerToken) {
-      setup(data.developerToken)
-    }
-  }, [])
 
   useEffect(() => {
     if (musicKit.authorizationToken && !musicKit.authorizationLoading) {
       navigate('/player')
     }
-  }, [musicKit.authorizationToken && !musicKit.authorizationLoading])
+  }, [musicKit.authorizationToken, musicKit.authorizationLoading])
 
   return (
     <Container display="flex" justify="center" alignItems="center" direction="column">
@@ -53,6 +24,6 @@ const Index = observer(() => {
       </Grid.Container>
     </Container>
   )
-})
+}
 
-export default Index
+export default observer(Index)
