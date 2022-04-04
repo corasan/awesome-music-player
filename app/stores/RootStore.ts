9655a1d19240f2fc72aaa88f3147159d2953649b
Henry Paulino <corasan.dev@gmail.com>
2022-04-04 00:20:05 -0400

    Update RootStore.ts

--- app/stores/RootStore.ts
+++ app/stores/RootStore.ts
@@ -1,13 +1,16 @@
 import { makeAutoObservable } from 'mobx'
 import MusicKitStore from './MusicKitStore'
+import PlayerStore from './PlayerStore'
 
 export default class RootStore {
   developerToken: string | null = null
   musicKit: MusicKitStore
+  player: PlayerStore
 
   constructor() {
     makeAutoObservable(this)
-    this.musicKit = new MusicKitStore()
+    this.musicKit = new MusicKitStore(this)
+    this.player = new PlayerStore(this)
   }
 
   setDeveloperToken = (value: string) => {
