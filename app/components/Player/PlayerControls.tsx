1ce099ad15424d02950c8219bef0fff42a53b164
Henry Paulino <corasan.dev@gmail.com>
2022-04-10 19:48:13 -0400

    add control icons

--- app/components/Player/PlayerControls.tsx
+++ app/components/Player/PlayerControls.tsx
@@ -1,12 +1,15 @@
-import { Progress, Grid, Text } from '@nextui-org/react'
+import { Progress, Row, Col, Text, Container } from '@nextui-org/react'
 import { observer } from 'mobx-react-lite'
 import { useEffect } from 'react'
 import { useStore } from '~/stores'
 import formatSeconds from '~/util/formatSeconds'
+import Pause from '../Svg/Pause'
+import Play from '../Svg/Play'
+import Step from '../Svg/Step'
 
 const PlayerControls = () => {
   const { player, musicKit } = useStore()
-  const opacity = player.nowPlaying ? 1 : 0.1
+  const opacity = player.nowPlaying ? 1 : 0.2
 
   useEffect(() => {
     if (musicKit.instance) {
@@ -19,30 +22,37 @@ const PlayerControls = () => {
   }, [musicKit.instance])
 
   return (
-    <Grid.Container direction="row" justify="space-evenly" alignItems="center" css={{ opacity }}>
-      <Grid xs={2} justify="center">
-        <Text>{!player.nowPlaying ? '--:--' : formatSeconds(player.playbackTime)}</Text>
-      </Grid>
-      <Grid lg>
-        {!player.nowPlaying ? (
-          <Progress value={0} color="gradient" size="xs" min={0} max={100} />
-        ) : (
-          <Progress
-            value={player.playbackProgress}
-            color="gradient"
-            size="xs"
-            min={0}
-            max={player.playbackDuration}
-            shadow
-          />
-        )}
-      </Grid>
-      <Grid xs={2} justify="center">
-        <Text>
-          {player.playbackDuration !== 0 ? formatSeconds(player.playbackDuration) : '--:--'}
-        </Text>
-      </Grid>
-    </Grid.Container>
+    <Container direction="column" justify="space-evenly" alignItems="center" css={{ opacity }}>
+      <Row align="center">
+        <Step />
+        {!player.isPlaying ? <Pause /> : <Play />}
+        <Step style={{ transform: 'rotate(180deg)' }} />
+      </Row>
+      <Row align="center">
+        <Col span={2} css={{ justifyContent: 'center', display: 'flex' }}>
+          <Text>{!player.nowPlaying ? '--:--' : formatSeconds(player.playbackTime)}</Text>
+        </Col>
+        <Col>
+          {!player.nowPlaying ? (
+            <Progress value={0} color="gradient" size="xs" min={0} max={100} />
+          ) : (
+            <Progress
+              value={player.playbackProgress}
+              color="gradient"
+              size="xs"
+              min={0}
+              max={player.playbackDuration}
+              shadow
+            />
+          )}
+        </Col>
+        <Col span={2} css={{ justifyContent: 'center', display: 'flex' }}>
+          <Text>
+            {player.playbackDuration !== 0 ? formatSeconds(player.playbackDuration) : '--:--'}
+          </Text>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
